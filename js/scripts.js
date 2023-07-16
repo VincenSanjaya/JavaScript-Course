2fd6e34d99dbade72b4940bb69c13fa06204a1a5
Vincen Sanjaya <vincenciogfs15@gmail.com>
2023-07-16 13:02:09 +0700

    Feat: [VG] Learning Variables

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,21 @@
 This is a multi-line comment
 */
 
+
+//***********************************************
 // Lesson 4 - DOM
+//***********************************************
 
-console.log(document.getElementById("blue_box").innerHTML);
+// console.log(document.getElementById("blue_box").innerHTML);
 
-document.getElementById("blue_box").innerHTML = "Hello World";
+// document.getElementById("blue_box").innerHTML = "Hello World";
 
-console.log(document.getElementById("blue_box").innerHTML);
+// console.log(document.getElementById("blue_box").innerHTML);
+
+
+//***********************************************
+// Lesson 5 - Variables
+//***********************************************
+var user_name = "Vincen";
+
+document.getElementById("user_greeting_message").innerHTML = "Hello " + user_name;
