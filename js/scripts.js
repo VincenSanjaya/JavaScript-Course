ae0e669913c3327b83f851240328320a7d3b2a7e
Vincen Sanjaya <vincenciogfs15@gmail.com>
2023-07-16 12:43:21 +0700

    Feat: [VG] Belajar Document Object Model

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// alert("testing the alert");
+
+// This is a single line comment
+
+/*
+This is a multi-line comment
+*/
+
+// Lesson 4 - DOM
+
+console.log(document.getElementById("blue_box").innerHTML);
+
+document.getElementById("blue_box").innerHTML = "Hello World";
+
+console.log(document.getElementById("blue_box").innerHTML);
